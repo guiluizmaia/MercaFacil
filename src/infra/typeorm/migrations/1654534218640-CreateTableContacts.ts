ccb675f6f7a899e3b9f06116456924c2f1efab1d
guiluizmaia <guilhermemaia201450@gmail.com>
2022-06-06 14:00:49 -0300

    feat: init configuration

---
+++ src/infra/typeorm/migrations/1654534218640-CreateTableContacts.ts
@@ -0,0 +1,51 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateTableContacts1654534218640 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const tableExists = await queryRunner.query(`
+            SELECT table_name 
+            FROM information_schema.tables
+            WHERE table_name='contacts'    
+        `);
+        
+        if (tableExists.length === 0) {
+            await queryRunner.createTable(
+              new Table({
+                name: "contacts",
+                columns: [
+                  {
+                    name: "id",
+                    type: "uuid",
+                    generationStrategy: "uuid",
+                    default: "uuid_generate_v4()",
+                    isPrimary: true,
+                  },
+                  {
+                    name: "nome",
+                    type: "varchar",
+                  },
+                  {
+                    name: "celular",
+                    type: "varchar",
+                  },
+                  {
+                    name: "created_at",
+                    type: "timestamp",
+                    default: "now()",
+                  },
+                  {
+                    name: "updated_at",
+                    type: "timestamp",
+                    default: "now()",
+                  },
+                ],
+              })
+            );
+          }
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
